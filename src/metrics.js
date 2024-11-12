170aaa9f9b325d1e876655713b2d5b40151189fd
mcottle1 <kenzcottle@gmail.com>
2024-11-11 19:28:18 -0700

    logins and logouts

--- src/metrics.js
+++ src/metrics.js
@@ -8,14 +8,16 @@ class Metrics {
     this.postRequests = 0;
     this.deleteRequests = 0;
     this.putRequests = 0;
-    this.activeUsers = 0;
-    this.successfulAuths = 0;
-    this.failedAuths = 0;
-    this.soldPizzas = 0;
-    this.failedPizzas = 0;
-    this.totalRevenue = 0;
-    this.totalRequestTimeSinceInterval = 0;
-    this.totalPizzaTimeSinceInterval = 0;
+    this.logins = 0;
+    this.logouts = 0;
+    // this.activeUsers = 0;
+    // this.successfulAuths = 0;
+    // this.failedAuths = 0;
+    // this.soldPizzas = 0;
+    // this.failedPizzas = 0;
+    // this.totalRevenue = 0;
+    // this.totalRequestTimeSinceInterval = 0;
+    // this.totalPizzaTimeSinceInterval = 0;
 
     // This will periodically sent metrics to Grafana
     const timer = setInterval(() => {
@@ -26,14 +28,15 @@ class Metrics {
       this.sendMetricToGrafana('request', 'put', 'put', this.putRequests);
       this.sendMetricToGrafana('cpu', 'all', 'usage', this.getCpuUsagePercentage());
       this.sendMetricToGrafana('memory', 'all', 'usage', this.getMemoryUsagePercentage());
-      this.sendMetricToGrafana('users', 'all', 'activeusers', this.activeUsers);
-      this.sendMetricToGrafana('auth', 'all', 'successful', this.successfulAuths);
-      this.sendMetricToGrafana('auth', 'all', 'failed', this.failedAuths);
-      this.sendMetricToGrafana('pizzas', 'all', 'soldpizzas', this.soldPizzas);
-      this.sendMetricToGrafana('pizzas', 'all', 'failedpizzas', this.failedPizzas);
-      this.sendMetricToGrafana('pizzas', 'all', 'totalrevenue', this.totalRevenue);
-      this.sendMetricToGrafana('latency', 'all', 'latency', this.totalRequestTimeSinceInterval);
-      this.sendMetricToGrafana('latency', 'all', 'pizzalatency', this.totalPizzaTimeSinceInterval);
+      this.sendMetricToGrafana('users', 'all', 'logins', this.logins);
+      this.sendMetricToGrafana('users', 'all', 'logouts', this.logouts);
+    //   this.sendMetricToGrafana('auth', 'all', 'successful', this.successfulAuths);
+    //   this.sendMetricToGrafana('auth', 'all', 'failed', this.failedAuths);
+    //   this.sendMetricToGrafana('pizzas', 'all', 'soldpizzas', this.soldPizzas);
+    //   this.sendMetricToGrafana('pizzas', 'all', 'failedpizzas', this.failedPizzas);
+    //   this.sendMetricToGrafana('pizzas', 'all', 'totalrevenue', this.totalRevenue);
+    //   this.sendMetricToGrafana('latency', 'all', 'latency', this.totalRequestTimeSinceInterval);
+    //   this.sendMetricToGrafana('latency', 'all', 'pizzalatency', this.totalPizzaTimeSinceInterval);
     }, 3000);
     timer.unref();
   }
@@ -58,41 +61,41 @@ class Metrics {
     this.putRequests++;
   }
 
-  incrementUsers() {
-    this.activeUsers++;
+  incrementLogins() {
+    this.logins++;
   }
 
-  decrementUsers() {
-    this.activeUsers--;
-  }
+  incrementLogouts() {
+    this.logouts++;
+}
 
-  incrementSuccessfulAuths() {
-    this.successfulAuths++;
-  }
+//   incrementSuccessfulAuths() {
+//     this.successfulAuths++;
+//   }
 
-  incrementFailedAuths() {
-    this.failedAuths++;
-  }
+//   incrementFailedAuths() {
+//     this.failedAuths++;
+//   }
 
-  incrementPizzas() {
-    this.soldPizzas++;
-  }
+//   incrementPizzas() {
+//     this.soldPizzas++;
+//   }
 
-  incrementFailedPizzas() {
-    this.failedPizzas++;
-  }
+//   incrementFailedPizzas() {
+//     this.failedPizzas++;
+//   }
 
-  incrementRevenue(revenue) {
-    this.successfulAuths = this.totalRevenue + revenue;
-  }
+//   incrementRevenue(revenue) {
+//     this.successfulAuths = this.totalRevenue + revenue;
+//   }
 
   incrementRequestTimeSinceInterval(duration) {
     this.totalRequestTimeSinceInterval = this.totalRequestTimeSinceInterval + duration;
   }
 
-  incrementPizzaRequestTimeSinceInterval(duration) {
-    this.totalPizzaTimeSinceInterval = this.totalPizzaTimeSinceInterval + duration;
-  }
+//   incrementPizzaRequestTimeSinceInterval(duration) {
+//     this.totalPizzaTimeSinceInterval = this.totalPizzaTimeSinceInterval + duration;
+//   }
 
   getCpuUsagePercentage() {
     const cpuUsage = os.loadavg()[0] / os.cpus().length;
@@ -134,13 +137,13 @@ class Metrics {
         if (req.method === 'DELETE') {
             metrics.incrementDeleteRequests();
             if (req.url === '/api/auth') {
-                metrics.decrementUsers();
+                metrics.incrementLogouts();
             }
         }
         if (req.method === 'PUT') {
             metrics.incrementPutRequests();
             if (req.url === '/api/auth') {
-                metrics.incrementUsers();
+                metrics.incrementLogins();
             }
         }
         // if (req.url === '/api/auth') {
