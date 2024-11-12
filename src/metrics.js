7b317c82c0199e9cee42eaae712910867854b476
mcottle1 <kenzcottle@gmail.com>
2024-11-11 19:46:32 -0700

    par down metrics

--- src/metrics.js
+++ src/metrics.js
@@ -9,17 +9,7 @@ class Metrics {
     this.deleteRequests = 0;
     this.putRequests = 0;
     this.logins = 0;
-    this.logouts = 0;
-    // this.activeUsers = 0;
-    // this.successfulAuths = 0;
-    // this.failedAuths = 0;
-    // this.soldPizzas = 0;
-    // this.failedPizzas = 0;
-    // this.totalRevenue = 0;
-    // this.totalRequestTimeSinceInterval = 0;
-    // this.totalPizzaTimeSinceInterval = 0;
 
-    // This will periodically sent metrics to Grafana
     const timer = setInterval(() => {
       this.sendMetricToGrafana('request', 'all', 'total', this.totalRequests);
       this.sendMetricToGrafana('request', 'get', 'get', this.getRequests);
@@ -29,14 +19,6 @@ class Metrics {
       this.sendMetricToGrafana('cpu', 'all', 'usage', this.getCpuUsagePercentage());
       this.sendMetricToGrafana('memory', 'all', 'usage', this.getMemoryUsagePercentage());
       this.sendMetricToGrafana('users', 'all', 'logins', this.logins);
-      this.sendMetricToGrafana('users', 'all', 'logouts', this.logouts);
-    //   this.sendMetricToGrafana('auth', 'all', 'successful', this.successfulAuths);
-    //   this.sendMetricToGrafana('auth', 'all', 'failed', this.failedAuths);
-    //   this.sendMetricToGrafana('pizzas', 'all', 'soldpizzas', this.soldPizzas);
-    //   this.sendMetricToGrafana('pizzas', 'all', 'failedpizzas', this.failedPizzas);
-    //   this.sendMetricToGrafana('pizzas', 'all', 'totalrevenue', this.totalRevenue);
-    //   this.sendMetricToGrafana('latency', 'all', 'latency', this.totalRequestTimeSinceInterval);
-    //   this.sendMetricToGrafana('latency', 'all', 'pizzalatency', this.totalPizzaTimeSinceInterval);
     }, 3000);
     timer.unref();
   }
@@ -65,37 +47,9 @@ class Metrics {
     this.logins++;
   }
 
-  incrementLogouts() {
-    this.logouts++;
-}
-
-//   incrementSuccessfulAuths() {
-//     this.successfulAuths++;
-//   }
-
-//   incrementFailedAuths() {
-//     this.failedAuths++;
-//   }
-
-//   incrementPizzas() {
-//     this.soldPizzas++;
-//   }
-
-//   incrementFailedPizzas() {
-//     this.failedPizzas++;
-//   }
-
-//   incrementRevenue(revenue) {
-//     this.successfulAuths = this.totalRevenue + revenue;
-//   }
-
   incrementRequestTimeSinceInterval(duration) {
     this.totalRequestTimeSinceInterval = this.totalRequestTimeSinceInterval + duration;
   }
-
-//   incrementPizzaRequestTimeSinceInterval(duration) {
-//     this.totalPizzaTimeSinceInterval = this.totalPizzaTimeSinceInterval + duration;
-//   }
 
   getCpuUsagePercentage() {
     const cpuUsage = os.loadavg()[0] / os.cpus().length;
@@ -136,38 +90,13 @@ class Metrics {
         }
         if (req.method === 'DELETE') {
             metrics.incrementDeleteRequests();
-            if (req.url === '/api/auth') {
-                metrics.incrementLogouts();
-            }
         }
         if (req.method === 'PUT') {
             metrics.incrementPutRequests();
-            if (req.url === '/api/auth') {
-                metrics.incrementLogins();
-            }
         }
-        // if (req.url === '/api/auth') {
-        //     metrics.decrementUsers();
-        // }
-        // if (req.url === '/api/auth') {
-        //     if(res.body.status === 200) {
-        //         metrics.incrementUsers();
-        //         metrics.incrementSuccessfulAuths();
-        //     }else{
-        //         metrics.incrementFailedAuths();
-        //     }
-        // }
-        // if (req.url === '/api/order') {
-        //     metrics.incrementPizzaRequestTimeSinceInterval(duration);
-        //     req.body.items.forEach(item => {
-        //         if (res.body.status === 200) {
-        //             metrics.incrementPizzas();
-        //             metrics.incrementRevenue(item.price);
-        //         } else{
-        //             metrics.incrementFailedPizzas();
-        //         }
-        //     });
-        // }
+        if (req.method === 'PUT' && req.url === '/api/auth') {
+            metrics.incrementLogins();
+        }
     });
 
     next();
